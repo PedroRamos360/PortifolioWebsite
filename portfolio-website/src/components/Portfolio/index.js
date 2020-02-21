2ca546005f1e819757e73e764898142c630f5fab
Pedro Ramos <ramosct70@hotmail.com>
2020-02-20 21:56:13 -0300

    Atualização

--- portfolio-website/src/components/Portfolio/index.js
+++ portfolio-website/src/components/Portfolio/index.js
@@ -13,9 +13,9 @@ export default class Projects extends Component {
                     <h2>Portfólio</h2>
                 </Jumbotron>
                 <header>
-                    <img src={"https://toivjon.files.wordpress.com/2017/11/javafx-pong.png?w=772"} alt={""}/>
+                    <img src={"./Pong.jpg"} alt={""}/>
                     <div className="repo-info">
-                        <p>My version of the game Pong made with python</p>
+                        <p>Jogo Pong feito em python com modo singleplayer e multyplayer</p>
                     </div>
                 </header>
                 <Button bsStyle="primary" href={''}>Acessar repositório no github</Button>
