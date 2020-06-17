30c3035ef7170daa891ab8bcda802ae3260ba35a
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-06-17 10:25:01 -0300

    Responsividade

--- portfolio-website/src/components/Home/index.js
+++ portfolio-website/src/components/Home/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
 import './index.css';
+import './responsive.css';
 import CustomNavbar from "../CustomNavbar";
+import Background from '../assets/Background.jpg'
 
 export default class Home extends Component {
     render() {
         return(
-            <div id="background">
-                <CustomNavbar />
+            <div>
+                <img class="background-image" src={Background} alt="background"/>
+                <CustomNavbar class="navbar" />
                 <Jumbotron>
                     <h2>Pedro Ramos</h2>
                     <p>Estudante no Ensino Médio do Colégio Santo Antônio em Estrela.</p>
