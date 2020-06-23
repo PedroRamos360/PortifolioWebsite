c24155e53b86383a331a8582c08db65383a5c24e
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-06-22 21:53:34 -0300

    responsividade página portfólio

--- portfolio-website/src/components/Portfolio/index.js
+++ portfolio-website/src/components/Portfolio/index.js
@@ -22,62 +22,57 @@ export default class Projects extends Component {
                     <h2>Portfólio</h2>
                 </Jumbotron>
                 <li className="repo-item">
-                    <header>
-                        <img class="item-img" src={Pong} alt={"PongImage"}/>
-                        <div className="repo-info">
-                            <p>Jogo Pong feito em python com modo singleplayer e multyplayer</p>
-                        </div>
+                    <header class="item">
                         <Button bsStyle="primary" href={'https://github.com/PedroRamos360/Pong'}>
-                        Acessar repositório no github
+                            <img class="item-img" src={Pong} alt={"PongImage"}/>
+                            <div className="repo-info">
+                                <p>Jogo Pong feito em python com modo singleplayer e multyplayer</p>
+                            </div>
                         </Button>
                     </header>
-                    <header>
-                        <img class="item-img" src={TreinadorAritmetico} alt={"TreinadorAritmetico"}/>
-                        <div className="repo-info">
-                            <p>
-                                Programa que eu criei com python que tem como objetivo ajudar o usuário
-                                a melhorar suas habilidades aritméticas
-                            </p>
-                        </div>
+                    <header class="item">
                         <Button className="btn" bsStyle="primary" href={'https://github.com/PedroRamos360/AritmeticTrainer'}>
-                        Acessar repositório no github
+                            <img class="item-img" src={TreinadorAritmetico} alt={"TreinadorAritmetico"}/>
+                            <div className="repo-info">
+                                <p>
+                                    Programa que eu criei com python que tem como objetivo ajudar o usuário
+                                    a melhorar suas habilidades aritméticas
+                                </p>
+                            </div>
                         </Button>
                     </header>
-                    <header>
-                        <img class="item-img" src={BeTheHero} alt={"BeTheHero"}/>
-                        <div className="repo-info">
-                            <p>
-                                Be The Hero. Aplicação comercial que eu fiz durante a semana Omnistack 11.
-                                Nela ongs podem se cadastrar para colocar casos que precisam de dinheiro e
-                                os usuários podem entrar e doar para ajudar.
-                            </p>
-                        </div>
+                    <header class="item">
                         <Button className="btn" bsStyle="primary" href={'https://github.com/PedroRamos360/BeTheHero-Omnistack11'}>
-                        Acessar repositório no github
+                            <img class="item-img" src={BeTheHero} alt={"BeTheHero"}/>
+                            <div className="repo-info">
+                                <p>
+                                    Be The Hero. Aplicação comercial que eu fiz durante a semana Omnistack 11.
+                                    Nela ongs podem se cadastrar para colocar casos que precisam de dinheiro e
+                                    os usuários podem entrar e doar para ajudar.
+                                </p>
+                            </div>
                         </Button>
                     </header>
-                    <header>
-                        <img class="item-img" src={ChestFounder} alt={"ChestFounder"}/>
-                        <div className="repo-info">
-                            <p>
-                                Jogo que eu fiz programado em C# usando a plataforma da Unity para desenvolvimento de jogos
-                            </p>
-                        </div>
+                    <header class="item">
                         <Button className="btn" bsStyle="primary" href={'https://github.com/PedroRamos360/ChestFounder'}>
-                        Acessar repositório no github
+                            <img class="item-img" src={ChestFounder} alt={"ChestFounder"}/>
+                            <div className="repo-info">
+                                <p>
+                                    Jogo que eu fiz programado em C# usando a plataforma da Unity para desenvolvimento de jogos
+                                </p>
+                            </div>
                         </Button>
                     </header>
-                    <header>
-                        <img class="item-img" src={DevRadar} alt={"DevRadar"}/>
-                        <div className="repo-info">
-                            <p>
-                                Dev Radar. Aplicação comercial que eu fiz durante a semana Omnistack 10.
-                                Nela desenvolvedores se cadastram e podem procurar outros desenvolvedores que
-                                ficam próximos a sua região.
-                            </p>
-                        </div>
+                    <header class="item">
                         <Button className="btn" bsStyle="primary" href={'https://github.com/PedroRamos360/DevRadar'}>
-                        Acessar repositório no github
+                            <img class="item-img" src={DevRadar} alt={"DevRadar"}/>
+                            <div className="repo-info">
+                                <p>
+                                    Dev Radar. Aplicação comercial que eu fiz durante a semana Omnistack 10.
+                                    Nela desenvolvedores se cadastram e podem procurar outros desenvolvedores que
+                                    ficam próximos a sua região.
+                                </p>
+                            </div>
                         </Button>
                     </header>
                 </li>
